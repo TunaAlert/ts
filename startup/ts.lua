b724fd598f4fe681c8ec17b91e97d673d0b67773
TunaAlert <30604068+TunaAlert@users.noreply.github.com>
2026-06-13 18:51:56 +0200

    Fetch repository data for programs loading
    Added functionality to fetch repository data from GitHub API.

--- startup/ts.lua
+++ startup/ts.lua
@@ -1,8 +1,20 @@
 local completion = require("cc.shell.completion")
 
-local programs = {}
---TODO: load programs
+local repos = {
+    {owner="TunaAlert", repo="ts", branch="main"}
+}
 
+local programs = {}
+
+for i, repo in pairs(repos) do
+    local request = http.get(("https://api.github.com/repos/%s/%s/git/trees/%s"):format(repo.owner, repo.repo, repo.branch)
+
+    --TODO: load response to programs. We retrieve the ts branch of the returned tree and request another tree from that hash. You'll get it.
+    print(request.readAll())
+    
+    request.close()
+end
+        
 shell.setAlias("ts", "/programs/ts.lua")
 
 shell.setCompletionFunction("programs/ts.lua", completion.build(
