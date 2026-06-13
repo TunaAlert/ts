536b20b74e70896ab6988fdec41c9092db57d9c7
TunaAlert <30604068+TunaAlert@users.noreply.github.com>
2026-06-13 19:12:05 +0200

    Alter ts command structure
    added option to list repos and files and to configure the used repos

--- startup/ts.lua
+++ startup/ts.lua
@@ -1,17 +1,21 @@
 local completion = require("cc.shell.completion")
 local ts = require("/programs/api/ts")
 
-
 local programs = ts.getPrograms()
 
 shell.setAlias("ts", "/programs/ts.lua")
 
 shell.setCompletionFunction("programs/ts.lua", completion.build(
-        {completion.choice, {"install", "remove", "upgrade", "update", "list"}},
+        {completion.choice, {"install", "remove", "upgrade", "update", "list", "config"}},
         function(shell, text, previous)
             if previous[2] == "install" or previous[2] == "remove" then
                 return completion.choice(shell, text, previous, programs)
+            elseif previous[2] == "list" then
+                return completion.choice(shell, text, previous, {"programs", "repos"})
+            elseif previous[2] == "config" then
+                return completion.choice(shell, text, previous, {"repo-add", "repo-remove"})
             end
             return nil
-        end
+        end,
+        
         ))
