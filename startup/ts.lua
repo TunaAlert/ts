5476dd0a9f6a9f64953d68bf2a606afbbded23f4
TunaAlert <30604068+TunaAlert@users.noreply.github.com>
2026-06-13 18:56:38 +0200

    Refactor ts.lua to use ts.getPrograms()
    Removed unused repository handling and integrated program retrieval from the ts module.

--- startup/ts.lua
+++ startup/ts.lua
@@ -1,20 +1,9 @@
 local completion = require("cc.shell.completion")
+local ts = require("/programs/api/ts")
 
-local repos = {
-    {owner="TunaAlert", repo="ts", branch="main"}
-}
 
-local programs = {}
+local programs = ts.getPrograms()
 
-for i, repo in pairs(repos) do
-    local request = http.get(("https://api.github.com/repos/%s/%s/git/trees/%s"):format(repo.owner, repo.repo, repo.branch)
-
-    --TODO: load response to programs. We retrieve the ts branch of the returned tree and request another tree from that hash. You'll get it.
-    print(request.readAll())
-    
-    request.close()
-end
-        
 shell.setAlias("ts", "/programs/ts.lua")
 
 shell.setCompletionFunction("programs/ts.lua", completion.build(
