dfc6b6bf2486295fc59f75164ab7bd121049a716
TunaAlert <30604068+TunaAlert@users.noreply.github.com>
2026-06-13 18:30:16 +0200

    Add shell alias and completion for ts.lua
    Set up shell alias and completion for ts.lua

--- startup/ts.lua
+++ startup/ts.lua
@@ -1,1 +1,16 @@
+local completion = require("cc.shell.completion")
 
+local programs = {}
+--TODO: load programs
+
+shell.setAlias("ts", "/programs/ts.lua")
+
+shell.setCompletionFunction("programs/ts.lua", completion.build(
+        {completion.choice, {"install", "remove", "upgrade", "update", "list"}},
+        function(shell, text, previous)
+            if previous[2] == "install" or previous[2] == "remove" then
+                return completion.choice(shell, text, previous, programs)
+            end
+            return nil
+        end
+        ))
