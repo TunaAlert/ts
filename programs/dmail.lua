9cac5fe8fadfc395762e0fde273557426c628457
TunaAlert <30604068+TunaAlert@users.noreply.github.com>
2026-06-14 07:07:26 +0200

    Create dmail.lua with test interface

---
+++ programs/dmail.lua
@@ -0,0 +1,70 @@
+local dmail = require("/programs/api/dmail")
+local yaml = require("/programs/api/yaml")
+
+local termWidth, termHeight = term.getSize()
+
+local config = yaml.load("/.data/dmail/config.yaml")
+if config == nil then
+    config = {
+        main_server = 20,
+        servers = {20}
+        }
+    yaml.save(config, "/.data/dmail/config.yaml")
+end
+
+local contacts = yaml.load("/.data/dmail/contacts.yaml")
+if contacts == nil then
+    contacts = {{name = "Tuna", id = 9}}
+    yaml.save(contacts, "/.data/dmail/contacts.yaml")
+end
+
+local function nameOrID(id)
+    for i, contact in contacts do
+        if contact.id == id then
+            return contact.name
+        end
+    end
+    return tostring(id)
+end
+
+local function displayDmailList(server, scroll)
+    shell.run("clear")
+    
+    local messageList = window.create(term.current(), 1, 3, termWidth, termHeight - 3)
+    local status = {}
+    local messages = {}
+    for i, server in pairs(config.servers) do
+        local s, m = dmail.fetch(server)
+        status[#status + 1] = s
+        for j, message in pairs(m) do
+            messages[#messages + 1] = message
+        end
+    end
+
+    local offset = 0
+    
+    for i, s in pairs(status) do
+        if s ~= dmail.SUCCESS then
+            messageList.setCursorPos(1, 1+offset)
+            messageList.write(("error status %d"):format(status))
+            offset = offset + 1
+        end
+    end
+    if #messages == 0 then
+        messageList.setCursorPos(1, 1 + offset)
+        messageList.write("No messages")
+    end
+    for i, message in pairs(messages) do
+        if i - scroll >= 1 and i - scroll <= termHeight - 3 then
+            messageList.setCursorPos(1, i-scroll + offset)
+            messageList.write(
+                ("%s %s %s"):format(
+                    "o",
+                    string.sub(nameOrID(message.sender) .. "      ", 1, 6),
+                    message.subject
+                ))
+        end
+    end
+end
+
+displayDmailList(settings.get("dmail.server"), 0)
