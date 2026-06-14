4cdc94a55fe43ce499da4011d50fd4d20395e8c7
TunaAlert <30604068+TunaAlert@users.noreply.github.com>
2026-06-14 13:42:18 +0200

    changed comma to period

--- programs/dmail.lua
+++ programs/dmail.lua
@@ -404,14 +404,14 @@ dmailListMenu = function()
                 menuButtonSelected[1] = (math.max(math.min(menuButtonSelected[1], #menuButtons), 1)) % #menuButtons + 1
                 menuButtonSelected[2] = math.max(math.min(menuButtonSelected[2], #menuButtons[menuButtonSelected[1]]), 1)
                 if menuButtonSelected[1] > 1 and menuButtonSelected[1] < #menuButtons then
-                    scroll = clampScrollInList(menuButtonSelected[1] - ({messageList,getSize()})[2] / 2)
+                    scroll = clampScrollInList(menuButtonSelected[1] - ({messageList.getSize()})[2] / 2)
                 end
                 displayDmailList()
             elseif key == "up" then
                 menuButtonSelected[1] = (math.max(math.min(menuButtonSelected[1], #menuButtons), 1) - 2) % #menuButtons + 1
                 menuButtonSelected[2] = math.max(math.min(menuButtonSelected[2], #menuButtons[menuButtonSelected[1]]), 1)
                 if menuButtonSelected[1] > 1 and menuButtonSelected[1] < #menuButtons then
-                    scroll = clampScrollInList(menuButtonSelected[1] - ({messageList,getSize()})[2] / 2)
+                    scroll = clampScrollInList(menuButtonSelected[1] - ({messageList.getSize()})[2] / 2)
                 end
                 displayDmailList()
             elseif key == "left" then
