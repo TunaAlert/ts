90d5dca3189ebd37e91be8692ee82fdbc440133e
TunaAlert <30604068+TunaAlert@users.noreply.github.com>
2026-06-15 16:26:00 +0200

    fixed empty line boundary condition

--- programs/dmail.lua
+++ programs/dmail.lua
@@ -944,7 +944,7 @@ composeDmailMenu = function()
             composeDmail()
         elseif event == "char" then
             local char = a
-            if menuButtonSelected[1] > 3 and menuButtonSelected[1] - 3 <= #composedMessage.lines then
+            if menuButtonSelected[1] > 3 and menuButtonSelected[1] - 4 <= #composedMessage.lines then
                 local index = getBodyPosInLine(composedmessage.body, termWidth - 1, menuButtonSelected[2], menuButtonSelected[1] - 3)
                 message.body = string.sub(message.body, 1, index) .. char .. string.sub(message.body, index + 1)
                 message.lines = getLines(message.body, termWidth - 1)
